341f4247060b4db34d2f725d68718f902d3779de
Arkadiusz Pikura <arkadiusz.pikura@gmail.com>
2024-11-17 22:33:12 +0100

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Arkadiusz Pikura on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
